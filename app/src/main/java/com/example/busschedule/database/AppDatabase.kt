beedb2d426e2d712e1e343e342d2698b878792b7
Andrey-Metelyov <andrey.metelyov@gmail.com>
2021-09-15 00:39:55 +0900

    it works!

--- app/src/main/java/com/example/busschedule/database/AppDatabase.kt
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -14,6 +14,7 @@ abstract class AppDatabase: RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
+
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
